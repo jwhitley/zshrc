cbc8b7a768ae8a652562034de97180ca0e1859cc
John Whitley <john@luminous-studios.com>
2024-02-08 14:35:25 -0800

    Migrate wt function to zsh-cdhelper plugin

--- .zplugins/zsh-cdhelper/zsh-cdhelper.zsh
+++ .zplugins/zsh-cdhelper/zsh-cdhelper.zsh
@@ -44,4 +44,36 @@ cdhelper() {
   compdef _$1 $1
 }
 
+# wt -- change to an adjacent git working tree
+wt() {
+  local toplevel
+  toplevel=$(git rev-parse --show-toplevel 2> /dev/null)
+  if [[ $? -gt 0 ]]; then
+    echo "Not in a git repo"
+    return
+  fi
+
+  local prefix=$(dirname "$toplevel")
+  if [[ $# -gt 0 ]]; then 
+    cd "$prefix/$1"
+  else
+    echo "No path passed; staying put"
+  fi
+}
+
+# Completion currently assumes that all worktrees share a common parent
+# directory
+_wt() {
+  local toplevel
+  toplevel=$(git rev-parse --show-toplevel 2> /dev/null)
+  if [[ $? -gt 0 ]]; then
+    _message "Not in a git repo"
+    return
+  fi
+
+  local prefix=$(dirname "$toplevel")
+ _path_files -/ -W "$prefix"
+}
+
+compdef _wt wt
 
